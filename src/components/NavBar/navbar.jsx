754dc88949fc633b43cbf93dcc5aa7081539d6c4
Santiago Paz <santipaz19.02@gmail.com>
2024-09-25 00:28:00 -0300

    Update navbar.jsx

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -4,11 +4,11 @@ import IconSearch from '../../assets/img/IconSearch.png';
 const Navbar = ({ searchText, setSearchText }) => {
     return (
         <div className='flex justify-center sm:justify-between w-full bg-[#FFCC00] text-[#2C3E50] h-20 items-center px-3'>
-            <img src={pokemonLogo} className='w-[10rem] h-fit hidden sm:block' alt='pokemon logo' />
+            <img src={pokemonLogo} className='w-[10rem] max-h-20 h-fit hidden sm:block' alt='pokemon logo' />
             <h1 className='text-3xl font-semibold hidden sm:block leading-10'>Battle of Pokemon</h1>
             <div className='sm:hidden flex gap-2 items-center justify-center'>
                 <h1 className='text-3xl font-semibold leading-10'>Battle of </h1>
-                <img src={pokemonLogo} className='w-[10rem] h-fit block sm:hidden' alt='pokemon logo' />
+                <img src={pokemonLogo} className='w-[10rem] max-h-20 h-fit block sm:hidden' alt='pokemon logo' />
             </div>
             <div className='items-center hidden sm:flex bg-white px-2 rounded-lg'>
                 <input
