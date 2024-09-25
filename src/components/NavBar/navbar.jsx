bec09a463fe8716f8135b00fb5f344f4d804f397
Santiago Paz <santipaz19.02@gmail.com>
2024-09-25 00:26:56 -0300

    fix responsive navbar

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -5,7 +5,11 @@ const Navbar = ({ searchText, setSearchText }) => {
     return (
         <div className='flex justify-center sm:justify-between w-full bg-[#FFCC00] text-[#2C3E50] h-20 items-center px-3'>
             <img src={pokemonLogo} className='w-[10rem] h-fit hidden sm:block' alt='pokemon logo' />
-            <h1 className='text-3xl font-semibold leading-10'>Battle of Pokemon</h1>
+            <h1 className='text-3xl font-semibold hidden sm:block leading-10'>Battle of Pokemon</h1>
+            <div className='sm:hidden flex gap-2 items-center justify-center'>
+                <h1 className='text-3xl font-semibold leading-10'>Battle of </h1>
+                <img src={pokemonLogo} className='w-[10rem] h-fit block sm:hidden' alt='pokemon logo' />
+            </div>
             <div className='items-center hidden sm:flex bg-white px-2 rounded-lg'>
                 <input
                     className='h-[2rem] outline-none text-[#2C3E50]'
